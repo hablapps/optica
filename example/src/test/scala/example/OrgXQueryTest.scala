ca662046f91c652def5d2989879236d2194ce902
Alfonso Roa <roaredalfonso@gmail.com>
2019-05-24 12:28:13 +0200

    XQuery Test

--- example/src/test/scala/example/OrgXQueryTest.scala
+++ example/src/test/scala/example/OrgXQueryTest.scala
@@ -5,6 +5,9 @@ import _root_.org.scalatest._
 import optica._
 import xquery._
 import org._
+import _root_.org.basex.core._
+import _root_.org.basex.query._
+import scala.collection.JavaConverters._
 
 class OrgXQueryTest extends FlatSpec with Matchers {
 
@@ -13,6 +16,22 @@ class OrgXQueryTest extends FlatSpec with Matchers {
 
   "Optica" should "translate differences into an XQuery expression" in {
     expertise("abstract").toString shouldBe """/xml/department[not(exists(employee[not(exists(task/tsk[. = "abstract"]))]))]/dpt"""
+  }
+
+  it should "work with a xml example" in {
+
+    def process(query: String)(filePath: String):List[String] = { // Create a query processor
+      val xml = s"""doc('$filePath')""".stripMargin
+      val str = s"""for $$x in $xml$query return data($$x)"""
+      val context = new Context()
+      new QueryProcessor(str, context).value().asScala.map(_.toString).toList
+    }
+
+    val query = expertise("abstract").toString
+
+    process(query)("example/src/test/resources/org" +
+      ".xml") shouldBe List(""""Quality"""", """"Research"""")
+
   }
 }
 
